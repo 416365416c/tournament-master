7c9e21a55d2b58802b824f206e58b6c384915382
Alan Alpert <aalpert@blackberry.com>
2014-03-06 15:52:34 -0800

    Fancy-fied the view.
    Now uses a custom list element element, which should be reusable
    for the next view. Views will just be stacked list items in classic
    HTML 4 style.

---
+++ BrickShell/js/list-element.js
@@ -0,0 +1,41 @@
+define([ 
+  'jquery', 
+  'underscore', 
+  'xtag', 
+], function($, _, xTag){ 
+    function doMe(ele) {
+        //build the element from its custom properties
+        ele.innerHTML = "<p>" + ele.title + " : " + ele.subTitle + " ; " + ele.time + "</p><img src=\"" + ele.image + "\" />"
+    }
+    xTag.register('a-listItem', {
+        extends: 'div',
+        lifecycle: { 
+            created:  function () {
+                this.made = false;
+            },
+            inserted: function () {
+                doMe(this);
+                this.made = true;
+            },
+
+            attributeChanged: function() {
+                if (this.made == true)
+                    doMe(this);
+            },
+        },
+        accessors: {
+            subTitle: {
+                attribute : {},
+            },
+            title: {
+                attribute : {},
+            },
+            time: {
+                attribute : {},
+            },
+            image: {
+                attribute : {},
+            },
+        }
+    });
+});
--- BrickShell/js/main.js
+++ BrickShell/js/main.js
@@ -6,9 +6,9 @@ require.config({
     underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
     lodash: 'libs/lodash/lodash', // alternative to underscore
     backbone: 'libs/backbone/backbone-min', // https://github.com/amdjs
-    sinon: 'libs/sinon/sinon.js',
-    brick: 'libs/brick/brick-1.0.0rc1.min.js',
-    xtag: 'libs/xtag/x-tag-components.js',
+    sinon: 'libs/sinon/sinon',
+    brick: 'libs/brick/brick-1.0.0rc1.min',
+    xtag: 'libs/brick/x-tag-components',
 
     // Require.js plugins
     text: 'libs/require/text',
@@ -21,20 +21,27 @@ require.config({
 
 require([
   'data-structures',
+  'list-element',
   'jquery', 
   'underscore', 
-  'backbone', 
-], function(App, $, _, Backbone){
+  'backbone',
+  'xtag',
+], function(App, ListElemRegistrationProbablyWontAccess, $, _, Backbone,X){
 	var views = new Array();
 	var TournamentView = Backbone.View.extend({ //TODO: refactor into separate file
 		initialize: function() {
 	        },
 		render: function() {
-			console.log("Hola");
-			var templ = document.getElementById("tournamentListCardRow");
-			templ.innerHTML = this.model.get("name") + "<br/>";
+		//	var templ = document.getElementById("tournamentListCardRow");
+		//	templ.innerHTML = "<a-listitem title=\""this.model.get("name")"\" + "<br/>";
 			this.$el.empty();
-			this.$el.append(templ.content.cloneNode(true));
+            var listItem = document.createElement("a-listitem");
+            listItem.title = this.model.get("name");
+            listItem.subTitle = this.model.get("subT");
+            listItem.time = this.model.get("time");
+            listItem.image = "img/sc2.png"; //this.model.get("name");
+            listItem.onclick = function() { alert("It's ALLIIIIIIIIIIIVE! Starting from " + listItem.time); }
+			this.el.appendChild(listItem);
 			return this;
 		},
 	});
@@ -43,6 +50,5 @@ require([
 		$("#initialList").append(newGuy.el);
 		views.push(newGuy);//maintain ref
 		newGuy.render();
-		console.log("Oh dear...");
 	});
 });
